72d02a6ffd662cd37bc3614716c3d937eca4398a
Kai Harries <kai.harries@gmail.com>
2018-10-30 09:07:24 +0100

    emacs: sort install calls

--- overlays/99_my-environment/cfg.emacs/default.nix
+++ overlays/99_my-environment/cfg.emacs/default.nix
@@ -14,11 +14,11 @@ stdenv.mkDerivation rec {
 
     install -Dm 444 $src/calendar.el              $out/target-home/DOT.config/emacs/calendar.el
     install -Dm 444 $src/emacs.el.ex              $out/target-home/DOT.config/emacs/emacs.el.ex
+    install -Dm 444 $src/email.el                 $out/target-home/DOT.config/emacs/email.el
     install -Dm 444 $src/evil.el                  $out/target-home/DOT.config/emacs/evil.el
     install -Dm 444 $src/ivy_et_al.el             $out/target-home/DOT.config/emacs/ivy_et_al.el
     install -Dm 444 $src/keybindings_and_hooks.el $out/target-home/DOT.config/emacs/keybindings_and_hooks.el
     install -Dm 444 $src/misc.el                  $out/target-home/DOT.config/emacs/misc.el
-    install -Dm 444 $src/email.el                 $out/target-home/DOT.config/emacs/email.el
     install -Dm 444 $src/packages.el              $out/target-home/DOT.config/emacs/packages.el
   '';
 }
