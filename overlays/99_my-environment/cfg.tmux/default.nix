4f9b2ef002c5d24c838c36265e843c63c4cbdcbe
Kai Harries <kai.harries@gmail.com>
2018-11-04 14:11:55 +0100

    Fix "Add notmuch and acpitool to the tmux..."

--- overlays/99_my-environment/cfg.tmux/default.nix
+++ overlays/99_my-environment/cfg.tmux/default.nix
@@ -12,7 +12,7 @@ stdenv.mkDerivation rec {
   installPhase = ''
     install -dm 755 $out/target-home
     substitute $src/tmux.conf $out/target-home/DOT.tmux.conf \
-               --subst-var-by acpitool ${notmuch}/bin/acpitool \
+               --subst-var-by acpitool ${acpitool}/bin/acpitool \
                --subst-var-by notmuch  ${notmuch}/bin/notmuch \
                --subst-var-by open     ${open}/share/tmux-plugins/open/open.tmux \
                --subst-var-by urlview  ${urlview}/share/tmux-plugins/urlview/urlview.tmux
