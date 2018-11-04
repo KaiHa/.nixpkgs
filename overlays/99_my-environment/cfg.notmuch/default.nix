aafe2a52a9e193d649365b3c7fa8511ec516751d
Kai Harries <kai.harries@gmail.com>
2018-11-04 13:43:51 +0100

    Make notmuchs pre-new hook executable

--- overlays/99_my-environment/cfg.notmuch/default.nix
+++ overlays/99_my-environment/cfg.notmuch/default.nix
@@ -13,7 +13,7 @@ stdenv.mkDerivation rec {
 
   installPhase = ''
     install -Dm 444 $src/notmuch-config "$out/target-home/DOT.notmuch-config"
-    install -Dm 444 $src/pre-new        "$out/target-home/DOT.mail/DOT.notmuch/hooks/pre-new"
+    install -Dm 755 $src/pre-new        "$out/target-home/DOT.mail/DOT.notmuch/hooks/pre-new"
     aescrypt -d -p "${secret}" -o "$out/target-home/DOT.mail/DOT.notmuch/hooks/post-new" "$src/post-new.aes"
     chmod 755 $out/target-home/DOT.mail/DOT.notmuch/hooks/post-new
   '';
