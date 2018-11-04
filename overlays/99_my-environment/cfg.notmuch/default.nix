bfb0544b1ca977cd0332cf69781a96cfdc67f30f
Kai Harries <kai.harries@gmail.com>
2018-11-04 16:40:36 +0100

    notmuch: add fetch-mail.{service,timer}

--- overlays/99_my-environment/cfg.notmuch/default.nix
+++ overlays/99_my-environment/cfg.notmuch/default.nix
@@ -1,4 +1,33 @@
-{stdenv, aescrypt}:
+{stdenv, aescrypt, bash, coreutils, glibcLocales, gmailieer, notmuch, tzdata, writeText}:
+
+let
+  fmService = writeText "fetch-mail.service" ''
+    [Unit]
+    Description=Fetch mail
+
+    [Service]
+    Environment="LOCALE_ARCHIVE=${glibcLocales}/lib/locale/locale-archive"
+    Environment="PATH=${bash}/bin:${gmailieer}/bin:${notmuch}/bin:${coreutils}/bin"
+    Environment="TZDIR=${tzdata}/share/zoneinfo"
+
+    ExecStart=${notmuch}/bin/notmuch new
+    Type=oneshot
+
+    [Install]
+    WantedBy=fetch-mail.timer
+  '';
+
+  fmTimer = writeText "fetch-mail.timer" ''
+    [Unit]
+
+    [Timer]
+    OnCalendar=*-*-* *:0/5:00
+    Unit=fetch-mail.service
+
+    [Install]
+    WantedBy=default.target
+  '';
+in
 
 stdenv.mkDerivation rec {
   name = "config-notmuch";
@@ -12,6 +41,12 @@ stdenv.mkDerivation rec {
   secret = import <secret>;
 
   installPhase = ''
+    install -dm 755 "$out/target-home/DOT.config/systemd/user/default.target.wants"
+    install -dm 755 "$out/target-home/DOT.config/systemd/user/fetch-mail.timer.wants"
+    install -Dm 444 ${fmService}        "$out/target-home/DOT.config/systemd/user/fetch-mail.service"
+    install -Dm 444 ${fmService}        "$out/target-home/DOT.config/systemd/user/fetch-mail.timer.wants/fetch-mail.service"
+    install -Dm 444 ${fmTimer}          "$out/target-home/DOT.config/systemd/user/fetch-mail.timer"
+    install -Dm 444 ${fmTimer}          "$out/target-home/DOT.config/systemd/user/default.target.wants/fetch-mail.timer"
     install -Dm 444 $src/notmuch-config "$out/target-home/DOT.notmuch-config"
     install -Dm 755 $src/pre-new        "$out/target-home/DOT.mail/DOT.notmuch/hooks/pre-new"
     aescrypt -d -p "${secret}" -o "$out/target-home/DOT.mail/DOT.notmuch/hooks/post-new" "$src/post-new.aes"
