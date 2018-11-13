14970c7d0cb823c47aa5372c8b09c98fab4967e3
Kai Harries <kai.harries@gmail.com>
2018-11-13 18:33:42 +0100

    Whitespace cleanup

--- overlays/99_my-environment/cfg.emacs/packages.el
+++ overlays/99_my-environment/cfg.emacs/packages.el
@@ -2,30 +2,30 @@
 
 (setq
  package-archives '(("melpa-stable" . "https://stable.melpa.org/packages/")
-                    ("melpa"        . "https://melpa.org/packages/")
-                    ("gnu"          . "https://elpa.gnu.org/packages/"))
+		    ("melpa"        . "https://melpa.org/packages/")
+		    ("gnu"          . "https://elpa.gnu.org/packages/"))
 
  package-selected-packages '(bitbake
 			     company-ghci
-                             counsel
-                             dante
-                             editorconfig
-                             evil
+			     counsel
+			     dante
+			     editorconfig
+			     evil
 			     flycheck-haskell
-                             flycheck-rust
-                             haskell-mode
-                             ivy
-                             ivy-historian
-                             ivy-hydra
-                             ivy-rich
-                             ivy-rtags
-                             magit
-                             markdown-mode
-                             nix-mode
-                             rtags
-                             rust-mode
-                             ;;scion
-                             yaml-mode))
+			     flycheck-rust
+			     haskell-mode
+			     ivy
+			     ivy-historian
+			     ivy-hydra
+			     ivy-rich
+			     ivy-rtags
+			     magit
+			     markdown-mode
+			     nix-mode
+			     rtags
+			     rust-mode
+			     ;;scion
+			     yaml-mode))
 
 (package-refresh-contents)
 (package-install-selected-packages)
