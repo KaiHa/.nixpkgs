d00ed24d90c73304cc0e7cbebe863f5af7440cbc
Kai Harries <kai.harries@gmail.com>
2018-12-15 18:09:22 +0100

    xmonad: create and fill the ~/.xmonad configuration directory

--- overlays/99_my-environment/cfg.xmonad/default.nix
+++ overlays/99_my-environment/cfg.xmonad/default.nix
@@ -1,4 +1,11 @@
-{ mkDerivation, base, stdenv, xmonad, xmonad-contrib }:
+{ mkDerivation, base, stdenv, xmonad, xmonad-contrib, writeText }:
+
+let
+  buildSh = writeText "build" ''
+    #!/usr/bin/env bash
+    true
+  '';
+in
 mkDerivation {
   pname = "myxmonad";
   version = "0.1.0.0";
@@ -11,5 +18,12 @@ mkDerivation {
   postPatch = ''
     substitute xmobarrc.in xmobarrc \
                --subst-var-by datapath $out/share/x86_64-linux-ghc-8.4.3/myxmonad-0.1.0.0
+  '';
+
+
+  postInstall = ''
+    install -dm 755 "$out/target-home/DOT.xmonad"
+    install -Dm 555 ${buildSh}  "$out/target-home/DOT.xmonad/build"
+    ln -s "$out/bin/xmonad-x86_64-linux" "$out/target-home/DOT.xmonad/xmonad-x86_64-linux"
   '';
 }
