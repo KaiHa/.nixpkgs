2dbe47cc179fa618d050fd4cc84f00babc977bc2
Kai Harries <kai.harries@gmail.com>
2018-12-17 22:36:52 +0100

    emacs: add function fix-diary-dates

--- overlays/99_my-environment/cfg.emacs/functions.el
+++ overlays/99_my-environment/cfg.emacs/functions.el
@@ -26,3 +26,27 @@
 	     "[\n\"]+"))
 	   (cmd (concat "aescrypt  -p " secret " " args)))
     (shell-command-on-region (point-min) (point-max) cmd buf)))
+
+
+(defun fix-diary-dates ()
+  "Change dates like 2019/01/30 to Jan 30, 2019"
+  (interactive)
+  (save-excursion
+    (goto-char (point-min))
+    (while (re-search-forward "^\\(20[0-9][0-9]\\)\\/\\([0-9]+\\)\\/\\([0-9]+\\)" nil t)
+      (replace-match (concat
+                      (pcase (match-string 2)
+                        ((or "1" "01") "Jan")
+                        ((or "2" "02") "Feb")
+                        ((or "3" "03") "Mar")
+                        ((or "4" "04") "Apr")
+                        ((or "5" "05") "May")
+                        ((or "6" "06") "Jun")
+                        ((or "7" "07") "Jul")
+                        ((or "8" "08") "Aug")
+                        ((or "9" "09") "Sep")
+                        (   "10"       "Oct")
+                        (   "11"       "Nov")
+                        (   "12"       "Dec"))
+                      " \\3, \\1")))))
+
