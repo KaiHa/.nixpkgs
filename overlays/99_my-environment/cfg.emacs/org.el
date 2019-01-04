c4e68bd40f8abc463be17205e60795e8d12e248f
Kai Harries <kai.harries@gmail.com>
2019-01-05 00:10:56 +0100

    emacs: move org/ into emacs.d/

--- overlays/99_my-environment/cfg.emacs/org.el
+++ overlays/99_my-environment/cfg.emacs/org.el
@@ -3,18 +3,18 @@
 (require 'org-protocol)
 
 (setq
- org-agenda-files '("~/org")
+ org-agenda-files '("~/.emacs.d/org")
  org-agenda-include-diary t
  org-babel-load-languages '((emacs-lisp . t) (shell . t))
  org-capture-templates
-      '(("b" "Bookmark" entry (file+headline "~/org/notes.org" "Bookmarks")
+      '(("b" "Bookmark" entry (file+headline "~/.emacs.d/org/notes.org" "Bookmarks")
          "* %?%i\n  %a")
-        ("e" "Event" entry (file+headline "~/org/events.org" "Events")
+        ("e" "Event" entry (file+headline "~/.emacs.d/org/events.org" "Events")
          "* %i\n  %^t%?\n  %a")
-        ("j" "Journal" entry (file+datetree "~/org/journal.org")
+        ("j" "Journal" entry (file+datetree "~/.emacs.d/org/journal.org")
          "* %?\nEntered on %U\n  %i\n  %a")
-        ("t" "Todo" entry (file+headline "~/org/tasks.org" "Tasks")
+        ("t" "Todo" entry (file+headline "~/.emacs.d/org/tasks.org" "Tasks")
          "* TODO %?\n  %i\n  %a"))
  org-catch-invisible-edits 'error
- org-default-notes-file "~/org/notes.org"
+ org-default-notes-file "~/.emacs.d/org/notes.org"
  org-fontify-done-headline t)
