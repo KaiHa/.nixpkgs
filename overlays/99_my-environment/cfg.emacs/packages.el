eacdf070d7d67a836aa4bf5602d0662ef505c3b4
Kai Harries <kai.harries@gmail.com>
2019-01-27 17:53:34 +0100

    Install undo-tree, cleanup whitespaces

--- overlays/99_my-environment/cfg.emacs/packages.el
+++ overlays/99_my-environment/cfg.emacs/packages.el
@@ -2,40 +2,41 @@
 
 (setq
  package-archives '(("melpa-stable" . "https://stable.melpa.org/packages/")
-		    ("melpa"        . "https://melpa.org/packages/")
-		    ("gnu"          . "https://elpa.gnu.org/packages/"))
+                    ("melpa"        . "https://melpa.org/packages/")
+                    ("gnu"          . "https://elpa.gnu.org/packages/"))
 
  package-selected-packages '(bitbake
                              calfw
                              calfw-cal
-			     company-ghci
-			     counsel
-			     dante
-			     editorconfig
-			     elfeed
-			     evil
-			     flycheck-haskell
-			     flycheck-rust
+                             company-ghci
+                             counsel
+                             dante
+                             editorconfig
+                             elfeed
+                             evil
+                             flycheck-haskell
+                             flycheck-rust
                              forecast
                              german-holidays
-			     haskell-mode
+                             haskell-mode
                              hl-todo
-			     ivy
-			     ivy-historian
-			     ivy-hydra
-			     ivy-rich
-			     ivy-rtags
+                             ivy
+                             ivy-historian
+                             ivy-hydra
+                             ivy-rich
+                             ivy-rtags
                              json-mode
-			     magit
+                             magit
                              magit-annex
-			     ;;magit-todos
-			     markdown-mode
-			     nix-mode
+                             ;;magit-todos
+                             markdown-mode
+                             nix-mode
                              pdf-tools
-			     rtags
-			     rust-mode
-			     ;;scion
-			     yaml-mode))
+                             rtags
+                             rust-mode
+                             ;;scion
+                             undo-tree
+                             yaml-mode))
 
 (package-refresh-contents)
 (package-install-selected-packages)
