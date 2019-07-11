8d8fd7ea221e5576b32154f0dbff0d87ecaed602
Kai Harries <kai.harries@gmail.com>
2019-07-11 22:13:36 +0200

    emacs/dante: drop configuration of dante-repl-command

--- overlays/99_my-environment/cfg.emacs/haskell.el
+++ overlays/99_my-environment/cfg.emacs/haskell.el
@@ -4,21 +4,6 @@
 
 (setq
 
- dante-repl-command-line-methods '(bare bare-ghci)
- dante-repl-command-line-methods-alist
-    '((bare .
-            #[257 "\300\207"
-                  [("cabal" "new-repl" dante-target "--builddir=dist/dante")]
-                  2 "
-
-(fn _)"])
-      (bare-ghci .
-                 #[257 "\300\207"
-                       [("ghci")]
-                       2 "
-
-(fn _)"]))
-
  haskell-compile-cabal-build-alt-command "cd %s && cabal clean -s && cabal new-build --ghc-option=-ferror-spans"
  haskell-compile-cabal-build-command     "cd %s &&                   cabal new-build --ghc-option=-ferror-spans"
  haskell-mode-hook '(company-mode
