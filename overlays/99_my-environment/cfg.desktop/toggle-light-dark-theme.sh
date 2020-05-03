dbdc0dac13c8b97d1cf54c8d7aeb7e5cbede1d34
Kai Harries <kai.harries@posteo.de>
2020-05-03 21:24:42 +0200

    desktop: toggle also gtk4 apps between light/dark

--- overlays/99_my-environment/cfg.desktop/toggle-light-dark-theme.sh
+++ overlays/99_my-environment/cfg.desktop/toggle-light-dark-theme.sh
@@ -11,8 +11,10 @@ then
     cat ${dark} > ${cfg}
     emacsclient --eval "(kai/theme-toggle 'dark)"
     gawk -i inplace '/gtk-application-prefer-dark-theme/ { print "gtk-application-prefer-dark-theme = true"; next }; { print }' ~/.config/gtk-3.0/settings.ini
+    gawk -i inplace '/gtk-application-prefer-dark-theme/ { print "gtk-application-prefer-dark-theme = true"; next }; { print }' ~/.config/gtk-4.0/settings.ini
 else
     cat ${light} > ${cfg}
     emacsclient --eval "(kai/theme-toggle 'light)"
     gawk -i inplace '/gtk-application-prefer-dark-theme/ { print "gtk-application-prefer-dark-theme = false"; next }; { print }' ~/.config/gtk-3.0/settings.ini
+    gawk -i inplace '/gtk-application-prefer-dark-theme/ { print "gtk-application-prefer-dark-theme = false"; next }; { print }' ~/.config/gtk-4.0/settings.ini
 fi
