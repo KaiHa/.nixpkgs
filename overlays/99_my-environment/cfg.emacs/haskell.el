25c993c1bc694cb2384c801473eefe6cb3c8cba8
Kai Harries <kai.harries@posteo.de>
2020-10-13 18:38:05 +0200

    emacs: drop unnecessary Haskell settings
    ... nix-shell-activate does a better job

--- overlays/99_my-environment/cfg.emacs/haskell.el
+++ overlays/99_my-environment/cfg.emacs/haskell.el
@@ -3,13 +3,9 @@
 (define-key interactive-haskell-mode-map (kbd "C-c C-c") 'haskell-compile)
 
 (setq
- dante-repl-command-line-methods '(impure-nix new-build bare bare-ghci)
- haskell-compile-cabal-build-alt-command "cd %s && mycabal new-clean && mycabal new-build --ghc-option=-ferror-spans"
- haskell-compile-cabal-build-command     "cd %s && mycabal new-build --ghc-option=-ferror-spans"
  haskell-hoogle-command "hoogle --count=60"
  haskell-mode-hook '(lsp)
  haskell-process-log t
- haskell-process-path-cabal "mycabal"
  haskell-process-type 'auto
  lsp-haskell-server-wrapper-function
    (lambda
