7f23e87e15a54ead22c165d540af14f7741d290d
Kai Harries <kai.harries@posteo.de>
2020-10-29 17:38:04 +0100

    emacs: update systemd service file

--- overlays/99_my-environment/cfg.emacs/default.nix
+++ overlays/99_my-environment/cfg.emacs/default.nix
@@ -2,19 +2,28 @@
 
 let
   emacsService = writeText "emacs.service" ''
+    ## If your Emacs is installed in a non-standard location, you may need
+    ## to copy this file to a standard directory, eg ~/.config/systemd/user/ .
+    ## If you install this file by hand, change the "Exec" lines below
+    ## to use absolute file names for the executables.
     [Unit]
-    Description=Emacs: the extensible, self-documenting text editor
+    Description=Emacs text editor
     Documentation=info:emacs man:emacs(1) https://gnu.org/software/emacs/
-
+    
     [Service]
     Environment="LOCALE_ARCHIVE=${glibcLocales}/lib/locale/locale-archive"
     Environment="TZDIR=${tzdata}/share/zoneinfo"
     Environment=PKG_CONFIG_PATH=$HOME/.nix-profile/lib/pkgconfig/
     Environment=C_INCLUDE_PATH=$HOME/.nix-profile/include/
-
+    
+    Type=notify
     ExecStart=${bash}/bin/bash -c 'source /etc/profile; PATH="$PATH:${mediainfo}/bin:${weechat}/bin" exec ${emacs}/bin/emacs --fg-daemon'
+    ExecStop=${emacs}/bin/emacsclient --eval "(kill-emacs)"
+    # The location of the SSH auth socket varies by distribution, and some
+    # set it from PAM, so don't override by default.
+    # Environment=SSH_AUTH_SOCK=%t/keyring/ssh
     Restart=on-failure
-
+    
     [Install]
     WantedBy=default.target
   '';
