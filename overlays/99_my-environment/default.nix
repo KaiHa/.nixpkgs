686c62d020ce81e68162685f81402b6f03e23f13
Kai Harries <kai.harries@posteo.de>
2020-10-31 15:41:14 +0100

    texlive: add lastpage

--- overlays/99_my-environment/default.nix
+++ overlays/99_my-environment/default.nix
@@ -11,7 +11,7 @@ rec {
     ]);
 
   mytexlive = super.texlive.combine {
-    inherit (super.texlive) enumitem scheme-medium fontawesome moderncv xcolor-solarized;
+    inherit (super.texlive) enumitem lastpage scheme-medium fontawesome moderncv xcolor-solarized;
   };
 
   weechat = super.weechat.override {
