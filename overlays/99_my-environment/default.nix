35ba4165b5677f9d155439d28b93a50511552ae0
Kai Harries <kai.harries@posteo.de>
2020-11-24 22:07:18 +0100

    Drop rclone

--- overlays/99_my-environment/default.nix
+++ overlays/99_my-environment/default.nix
@@ -37,7 +37,6 @@ rec {
   _cfg-git           = super.callPackage ./cfg.git           {};
   _cfg-notmuch       = super.callPackage ./cfg.notmuch       {};
   _cfg-mc            = super.callPackage ./cfg.mc            {};
-  _cfg-rclone        = super.callPackage ./cfg.rclone        {};
   _cfg-redshift      = super.callPackage ./cfg.redshift      {};
   _cfg-ssh           = super.callPackage ./cfg.ssh           {};
   _cfg-tmux          = super.callPackage ./cfg.tmux          { inherit (self.tmuxPlugins) open urlview; };
@@ -128,10 +127,8 @@ rec {
     paths = [
       _cfg-emacs-private
       _cfg-notmuch
-      _cfg-rclone
       _cfg-vdirsyncer
       _cfg-ssh
-      rclone
       vdirsyncer
     ];
   };
