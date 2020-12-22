d0ea1bc36d91d90e53dcb09331570414c17b6f9f
Kai Harries <kai.harries@posteo.de>
2020-12-22 08:38:04 +0100

    toggle-light-dark-theme.sh: read current state from gtk settings.ini

--- overlays/99_my-environment/cfg.desktop/toggle-light-dark-theme.sh
+++ overlays/99_my-environment/cfg.desktop/toggle-light-dark-theme.sh
@@ -6,15 +6,15 @@ cfg=~/.config/alacritty/alacritty.yml
 dark=~/.config/alacritty/alacritty-dark.yml
 light=~/.config/alacritty/alacritty-light.yml
 
-if cmp ${cfg} ${light}
+if [[ $(awk '/gtk-application-prefer-dark-theme/ { print $3 }' ~/.config/gtk-3.0/settings.ini) = false ]]
 then
     cat ${dark} > ${cfg}
-    emacsclient --eval "(kai/theme-toggle 'dark)"
+    emacsclient --eval "(kai/theme-toggle 'dark)" && true
     gawk -i inplace '/gtk-application-prefer-dark-theme/ { print "gtk-application-prefer-dark-theme = true"; next }; { print }' ~/.config/gtk-3.0/settings.ini
     gawk -i inplace '/gtk-application-prefer-dark-theme/ { print "gtk-application-prefer-dark-theme = true"; next }; { print }' ~/.config/gtk-4.0/settings.ini
 else
     cat ${light} > ${cfg}
-    emacsclient --eval "(kai/theme-toggle 'light)"
+    emacsclient --eval "(kai/theme-toggle 'light)" && true
     gawk -i inplace '/gtk-application-prefer-dark-theme/ { print "gtk-application-prefer-dark-theme = false"; next }; { print }' ~/.config/gtk-3.0/settings.ini
     gawk -i inplace '/gtk-application-prefer-dark-theme/ { print "gtk-application-prefer-dark-theme = false"; next }; { print }' ~/.config/gtk-4.0/settings.ini
 fi
